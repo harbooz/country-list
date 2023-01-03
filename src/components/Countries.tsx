bdc1edbe5f6430bd9f19c36a12cfecf3c2701a01
Harbooz <alin.stoica80@gmail.com>
2023-01-03 19:13:49 +0200

    My first commit

---
+++ src/components/Countries.tsx
@@ -0,0 +1,50 @@
+import { useQuery } from "graphql-hooks";
+
+const HOMEPAGE_QUERY = `query {
+    countryResolver {
+      name,
+      code,
+      iso3,
+      dialCode,
+      defaultTimezone,
+      otpInAppEnabled
+    }
+  }
+  `;
+
+
+export default function CountriesComponent(): React.ReactElement {
+  const { loading, error, data } = useQuery(HOMEPAGE_QUERY);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Something Bad Happened</div>;
+  console.log(data.countryResolver)
+  return (
+    <ul className="country__items">
+      {data.countryResolver.map(({ name, code, iso3, otpInAppEnabled, defaultTimezone }: any, index: number) => (
+        <li key={index}>
+         
+          <div className={  otpInAppEnabled ? "country__row" : "country__row not--valid" }>
+            <span className="bar"></span>
+            <span className="flag__icon"><img alt="country flag" src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${code}.svg`}/></span>
+            <div className="row__content">
+              <div className="row__group">
+                <span>{name}</span>
+                <span>Timezone {defaultTimezone}</span>
+              </div>
+              <div className="country__code">
+                Country code:<span>({iso3})</span>
+              </div>
+            </div>
+            <div className="valid"></div>
+            <div className="not__valid"></div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
+
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+            <h3 className="header__title">Virta platform compatible countries</h3>
+        </header>
+  );
+}
+
+export default Header;
