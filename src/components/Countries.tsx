05632916d9ccae88f39dbbf42d386133340937c6
Alin Stoica <Alin.Stoica@888holdings.com>
2023-01-04 11:00:33 +0200

    Update

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -12,39 +12,50 @@ const HOMEPAGE_QUERY = `query {
   }
   `;
 
-
 export default function CountriesComponent(): React.ReactElement {
   const { loading, error, data } = useQuery(HOMEPAGE_QUERY);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Something Bad Happened</div>;
-  console.log(data.countryResolver)
+  console.log(data.countryResolver);
   return (
     <ul className="country__items">
-      {data.countryResolver.map(({ name, code, iso3, otpInAppEnabled, defaultTimezone }: any, index: number) => (
-        <li key={index}>
-         
-          <div className={  otpInAppEnabled ? "country__row" : "country__row not--valid" }>
-            <span className="bar"></span>
-            <span className="flag__icon"><img alt="country flag" src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${code}.svg`}/></span>
-            <div className="row__content">
-              <div className="row__group">
-                <span>{name}</span>
-                <span>Timezone {defaultTimezone}</span>
+      {data.countryResolver.map(
+        (
+          { name, code, iso3, otpInAppEnabled, defaultTimezone }: any,
+          index: number
+        ) => (
+          <li key={index}>
+            <div className="country__row">
+              <span
+                className="bar"
+                style={{ background: otpInAppEnabled ? "#00ad09" : "#ff0000" }}
+              ></span>
+              <span className="flag__icon">
+                <img
+                  alt="country flag"
+                  src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${code}.svg`}
+                />
+              </span>
+              <div className="row__content">
+                <div className="row__group">
+                  <span>{name}</span>
+                  <span>Timezone {defaultTimezone}</span>
+                </div>
+                <div className="country__code">
+                  Country code:<span>({iso3})</span>
+                </div>
               </div>
-              <div className="country__code">
-                Country code:<span>({iso3})</span>
-              </div>
+
+              {otpInAppEnabled ? (
+                <div className="valid"></div>
+              ) : (
+                <div className="not__valid"></div>
+              )}
             </div>
-            <div className="valid"></div>
-            <div className="not__valid"></div>
-          </div>
-        </li>
-      ))}
+          </li>
+        )
+      )}
     </ul>
   );
 }
-
-
-
-
